39a4bc402764201e92f4f7185a8be866c8b789e7
AbdurRahman1337 <abdurrahman978728@gmail.com>
2025-07-03 02:40:58 -0700

    adding files

---
+++ dom.js
@@ -0,0 +1,8 @@
+
+// const mydiv=document.createElement('div');
+// mydiv.style.cssText='width:40px;height:50px;background-color:red;';
+// document.body.appendChild(mydiv)
+
+const myDiv = document.createElement('div');
+myDiv.classList.add('johnthedon');
+document.body.appendChild(myDiv);
